703654cb4c95498314bc3206b10cbf7e204449d2
Eric Kanski <erickanski0402@gmail.com>
2020-03-31 22:28:13 -0500

    Plots out the bones of possible implementation for min/max

--- PlayerAI.py
+++ PlayerAI.py
@@ -1,4 +1,4 @@
-from random import randint
+from random import randint, random
 from BaseAI import BaseAI
 from math import inf
 
@@ -7,53 +7,77 @@ DOWN = 1
 LEFT = 2
 RIGHT = 3
 
+
+##################################DELETE ME#####################################
+################################################################################
+def winDisplay(grid):
+    for i in range(grid.size):
+        for j in range(grid.size):
+            print("%6d  " % grid.map[i][j], end="")
+        print("")
+    print("")
+################################################################################
+################################################################################
+
 class PlayerAI(BaseAI):
     def __init__(self):
         pass
 
     def getMove(self, gridCopy):
         dir = self.decision(gridCopy)
-        moves = gridCopy.getAvailableMoves()
+        
+        moves = sorted(gridCopy.getAvailableMoves(), key = lambda x: random())
         return moves[randint(0, len(moves ) - 1)] if moves else None
 
 
-    # Cut off the recursion at some depth
-    def minimize(self, gridCopy):
-        moves = gridCopy.getAvailableMoves()
-        if len(moves) is 0:
-            return (None, gridCopy.getMaxTile())
+    def minimize(self, state):
+        if self.terminalTest(state):
+            return (None, self.eval(state))
 
         (minChild, minUtility) = (None, inf)
 
-        for dir in moves:
-            child = gridCopy.clone()
-            child.move(dir)
-
-            (_, utility) = self.maximize(child)
-
-            if utility < minUtility:
-                (minChild, minUtility) = (child, utility)
-
+        # cells = all possible next grids that:
+        #   - Don't increase utility
+        #
+        # for <child> (grid object) in <calculated minimizing cells> (List of grid objects):
+        #   (_, utility) = Maximize(child)
+        #
+        #   if utility < minUtility:
+        #       (minChild, minUtility) = (child, utility)
+        #
         return (minChild, minUtility)
 
-    def maximize(self, gridCopy):
-        moves = gridCopy.getAvailableMoves()
-        if len(moves) is 0:
-            return (None, gridCopy.getMaxTile())
+    def maximize(self, state):
+        if self.terminalTest(state):
+            return (None, self.eval(state))
 
         (maxChild, maxUtility) = (None, -inf)
 
-        for dir in moves:
-            child = gridCopy.clone()
-            child.move(dir)
-
-            (_, utility) = self.minimize(child)
-
-            if utility > maxUtility:
-                (maxChild, maxUtility) = (child, utility)
-
+        # moves = calculate grids possible given all legal moves
+        # for <move> (grid object) in <all legal moves> (list of grid objects):
+        #   (_, utility) = Minimize(move)
+        #
+        #   if utility > maxUtility:
+        #       (maxChild, minChild) = (child, utility)
+        #
         return (maxChild, maxUtility)
 
     def decision(self, gridCopy):
         (child, _) = self.maximize(gridCopy)
         return child
+
+    def terminalTest(self, state):
+        # return a test of the following values:
+        #   - No available moves remain
+        #   -
+        return False
+
+    def eval(state):
+        # Ideas for utility function:
+        #   -number of merges for a given direction
+        #       i.e.: moving right results in 2 merges, whereas moving up results in 3 merges
+        #   -Number of open spaces after moving a direction:
+        #       i.e.: moving up results in 5 open spaces, whereas moving up results in only 2 open spaces
+        #  - Increase in score based on merges
+        #       i.e.: merging 2 and 2 adds 4 to the score, 4 and 4 yields 8, etc.
+        return 0
